fb14555f4549c7987762f0c6ef8ded3c2f025230
phenol94 <phenolate93@gmail.com>
2017-09-16 01:24:18 +0900

    거리에따른 메시지연동기능 추가

--- app/src/main/java/com/flea/android/fleaandroid/MainActivity.java
+++ app/src/main/java/com/flea/android/fleaandroid/MainActivity.java
@@ -1,8 +1,11 @@
 package com.flea.android.fleaandroid;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
@@ -11,11 +14,14 @@ import com.estimote.sdk.SystemRequirementsChecker;
 
 import java.util.List;
 
+import static android.media.CamcorderProfile.get;
+
 public class MainActivity extends AppCompatActivity {
     private BeaconManager beaconManager;
     private static final Region ALL_ESTIMOTE_BEACONS_REGION = new Region("rid", null, null, null);
 
     private TextView tvId;
+    private Boolean isConnected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         tvId = (TextView) findViewById(R.id.tvId);
 
         beaconManager = new BeaconManager(getApplicationContext());
+        isConnected = false;
+
         beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
             @Override
             public void onServiceReady() {
@@ -35,19 +43,29 @@ public class MainActivity extends AppCompatActivity {
         beaconManager.setRangingListener(new BeaconManager.RangingListener() {
             @Override
             public void onBeaconsDiscovered(Region region, List<Beacon> list) {
-                /*
-                while (list.size() < 3) {
-                    Toast.makeText(getApplicationContext(), "" + list.size(), Toast.LENGTH_LONG).show();
+                Beacon currentBeacon = list.get(0);
+
+                tvId.setText(currentBeacon.getRssi() + "");
+
+                if(!isConnected & currentBeacon.getRssi() > -60) {
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+
+                    isConnected = true;
+
+                    dialog.setTitle("알림")
+                            .setMessage("비콘이 연결되었습니다.")
+                            .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                @Override public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            })
+                            .create().show();
+
+                } else if (isConnected & currentBeacon.getRssi() <= -60) {
+                    Toast.makeText(MainActivity.this, "연결이 끊어졌습니다.", Toast.LENGTH_SHORT).show();
+                    isConnected = false;
                 }
-                */
 
-                Beacon currentBeacon1 = list.get(0);
-                //Beacon currentBeacon2 = list.get(1);
-                //Beacon currentBeacon3 = list.get(2);
-
-                //tvId.setText(currentBeacon1.getRssi() + " " + currentBeacon2.getRssi() + " " + currentBeacon3.getRssi());
-
-                tvId.setText(currentBeacon1.getRssi() + " ");
             }
         });
     }
