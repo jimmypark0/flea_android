481b665f80204510380ed5ecdea186e4adaf492b
JerryJung-MacBook Pro <jwh1269@naver.com>
2017-09-16 14:45:38 +0900

    네비게이션 아이콘들 추가 및 로고변경 및 소스정리

--- app/src/main/java/com/flea/android/fleaandroid/MainActivity.java
+++ app/src/main/java/com/flea/android/fleaandroid/MainActivity.java
@@ -1,36 +1,49 @@
 package com.flea.android.fleaandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity
+        extends BaseActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout mDrawerLayout;
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.map:
+            case R.id.action_map:
                 Toast.makeText(getApplicationContext(), "Clicked MAP", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.event_list:
+                return true;
+            case R.id.action_event_list:
                 Toast.makeText(getApplicationContext(), "Clicked 이벤트들", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.vibration:
+                return true;
+            case R.id.action_vibration:
                 Toast.makeText(getApplicationContext(), "Clicked 진 동", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.notification:
+                return true;
+            case R.id.action_notification:
                 Toast.makeText(getApplicationContext(), "Clicked 알 림", Toast.LENGTH_SHORT).show();
-                break;
+                return true;
         }
         return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 
     @Override
@@ -57,14 +70,23 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+            return;
+        }
 
-
+        super.onBackPressed();
     }
 
+    // 검색화면으로 이동하는 작업
     @Override
-    protected void onPause() {
-        super.onPause();
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                startActivity(new Intent(getApplicationContext(), SearchListActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
