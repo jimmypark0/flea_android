a502cc4044bbed1f5b2ad060b108552bc643cbfa
JerryJung-MacBook Pro <jwh1269@naver.com>
2017-09-16 11:21:48 +0900

    Firebase Example Source Code 추가

--- app/src/main/java/com/flea/android/fleaandroid/MainActivity.java
+++ app/src/main/java/com/flea/android/fleaandroid/MainActivity.java
@@ -27,11 +27,31 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //Firebase Write Example Source Code
         /*// Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");*/
+        
+        //Firebase Read Example Source Code
+        /*// Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });*/
 
         /*tvId = (TextView) findViewById(R.id.tvId);
 
