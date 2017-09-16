97441c8798f7b08fbc01bbc349d57a30981e2e0f
phenol94 <phenolate93@gmail.com>
2017-09-16 15:24:07 +0900

    비콘 서비스 onStartCommand, onDestroy 추가 작성

--- app/src/main/java/com/flea/android/fleaandroid/BeaconService.java
+++ app/src/main/java/com/flea/android/fleaandroid/BeaconService.java
@@ -14,9 +14,10 @@ import java.util.List;
 public class BeaconService extends Service {
     private BeaconManager beaconManager;
     private static final Region ALL_ESTIMOTE_BEACONS_REGION = new Region("rid", null, null, null);
+    private static final int beaconMinors[] = {38547, 16501, 978};
     //private final IBinder mBinder = new LocalServiceBinder();
 
-    HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+    HashMap<Integer, Boolean> beaconConnectMap = new HashMap<Integer, Boolean>();
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -35,5 +36,37 @@ public class BeaconService extends Service {
                 beaconManager.startRanging(ALL_ESTIMOTE_BEACONS_REGION);
             }
         });
+
+        for (int i = 0; i < beaconMinors.length; i++) {
+            beaconConnectMap.put(beaconMinors[i], false);
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, List<Beacon> list) {
+                for (int i = 0; i < list.size(); i++) {
+
+                    Beacon currentBeacon = list.get(i);
+
+                    if (!beaconConnectMap.get(currentBeacon.getMinor()) & currentBeacon.getRssi() > -70) {
+                        beaconConnectMap.put(currentBeacon.getMinor(), true);
+
+                    } else if (beaconConnectMap.get(currentBeacon.getMinor()) & currentBeacon.getRssi() <= -70) {
+                        beaconConnectMap.put(currentBeacon.getMinor(), false);
+                    }
+                }
+            }
+        });
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        beaconManager.stopRanging(ALL_ESTIMOTE_BEACONS_REGION);
     }
 }
