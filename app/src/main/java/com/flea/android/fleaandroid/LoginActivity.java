ea99c77f18836ae7577c40872feab88373c8e08d
JerryJung-MacBook Pro <jwh1269@naver.com>
2017-09-16 13:31:41 +0900

    블루투스 권한 로그인쪽으로 변경 및 메인페이지에 네이게이션바 구축완료 및 툴바 커스텀 추가

--- app/src/main/java/com/flea/android/fleaandroid/LoginActivity.java
+++ app/src/main/java/com/flea/android/fleaandroid/LoginActivity.java
@@ -5,12 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.estimote.sdk.SystemRequirementsChecker;
+
 public class LoginActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 블루투스 권한 및 활성화 코드
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
     }
 
     public void customOnClick(View view) {
--- app/src/main/java/com/flea/android/fleaandroid/MainActivity.java
+++ app/src/main/java/com/flea/android/fleaandroid/MainActivity.java
@@ -1,117 +1,70 @@
 package com.flea.android.fleaandroid;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
-import com.estimote.sdk.Beacon;
-import com.estimote.sdk.BeaconManager;
-import com.estimote.sdk.Region;
-import com.estimote.sdk.SystemRequirementsChecker;
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout mDrawerLayout;
 
-import java.util.HashMap;
-import java.util.List;
-
-public class MainActivity extends BaseActivity {
-    /*private BeaconManager beaconManager;
-    private static final Region ALL_ESTIMOTE_BEACONS_REGION = new Region("rid", null, null, null);
-
-    private TextView tvId;
-    HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();*/
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.map:
+                Toast.makeText(getApplicationContext(), "Clicked MAP", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.event_list:
+                Toast.makeText(getApplicationContext(), "Clicked 이벤트들", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.vibration:
+                Toast.makeText(getApplicationContext(), "Clicked 진 동", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.notification:
+                Toast.makeText(getApplicationContext(), "Clicked 알 림", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Toolbar toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
 
-        //Firebase Write Example Source Code
-        /*// Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
 
-        myRef.setValue("Hello, World!");*/
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
-        //Firebase Read Example Source Code
-        /*// Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });*/
-
-        /*tvId = (TextView) findViewById(R.id.tvId);
-
-        beaconManager = new BeaconManager(getApplicationContext());
-
-        map.put(38547, false);
-        map.put(16501, false);
-        map.put(978, false);
-
-        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
-            @Override
-            public void onServiceReady() {
-                beaconManager.startRanging(ALL_ESTIMOTE_BEACONS_REGION);
-            }
-        });
-
-        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
-            @Override
-            public void onBeaconsDiscovered(Region region, List<Beacon> list) {
-                for (int i = 0; i < list.size(); i++) {
-
-                    Beacon currentBeacon = list.get(i);
-                    tvId.setText(currentBeacon.getRssi() + " / Minor : " + currentBeacon.getMinor());
-                    tvId.append("\n" + map);
-
-                    if (!map.get(currentBeacon.getMinor()) & currentBeacon.getRssi() > -70) {
-                        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
-
-                        map.put(currentBeacon.getMinor(), true);
-
-                        dialog.setTitle("알림")
-                                .setMessage(currentBeacon.getMinor() + " 비콘이 연결되었습니다.")
-                                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        // Test
-                                    }
-                                })
-                                .create().show();
-
-                    } else if (map.get(currentBeacon.getMinor()) & currentBeacon.getRssi() <= -70) {
-                        Toast.makeText(MainActivity.this, currentBeacon.getMinor() + " 비콘 연결이 끊어졌습니다.", Toast.LENGTH_SHORT).show();
-                        map.put(currentBeacon.getMinor(), false);
-                    }
-                }
-            }
-        });*/
+        final NavigationView leftNavigationView = findViewById(R.id.left_navigation);
+        leftNavigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        // 블루투스 권한 및 활성화 코드
-        //SystemRequirementsChecker.checkWithDefaultDialogs(this);
+
     }
 
     @Override
     protected void onPause() {
-        //beaconManager.stopRanging(ALL_ESTIMOTE_BEACONS_REGION);
         super.onPause();
     }
 }
