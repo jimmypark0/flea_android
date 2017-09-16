7d046df9ed14c753962619e5266d10134d1cf315
phenol94 <phenolate93@gmail.com>
2017-09-16 15:13:56 +0900

    비콘 서비스 onCreate 작성

--- app/src/main/java/com/flea/android/fleaandroid/BeaconService.java
+++ app/src/main/java/com/flea/android/fleaandroid/BeaconService.java
@@ -4,13 +4,36 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class BeaconService extends Service {
-    public BeaconService() {
-    }
+    private BeaconManager beaconManager;
+    private static final Region ALL_ESTIMOTE_BEACONS_REGION = new Region("rid", null, null, null);
+    //private final IBinder mBinder = new LocalServiceBinder();
+
+    HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        beaconManager = new BeaconManager(getApplicationContext());
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startRanging(ALL_ESTIMOTE_BEACONS_REGION);
+            }
+        });
     }
 }
