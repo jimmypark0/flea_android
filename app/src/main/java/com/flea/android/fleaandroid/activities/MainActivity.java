2f19fb0bfb860a1779c502fa75ea196d01deefcc
JerryJung-MacBook Pro <jwh1269@naver.com>
2017-09-16 21:20:46 +0900

    메인화면 전반적 디자인 완료

--- app/src/main/java/com/flea/android/fleaandroid/activities/MainActivity.java
+++ app/src/main/java/com/flea/android/fleaandroid/activities/MainActivity.java
@@ -1,9 +1,12 @@
 package com.flea.android.fleaandroid.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -12,11 +15,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.flea.android.fleaandroid.R;
 import com.flea.android.fleaandroid.utils.BaseActivity;
 import com.flea.android.fleaandroid.utils.MainDialog;
+import com.tistory.dwfox.dwrulerviewlibrary.view.DWRulerSeekbar;
+import com.tistory.dwfox.dwrulerviewlibrary.view.ScrollingValuePicker;
+
+import jp.wasabeef.blurry.Blurry;
 
 public class MainActivity
         extends BaseActivity
@@ -28,6 +37,8 @@ public class MainActivity
             mMainDialog.dismiss();
         }
     };
+    private Boolean isFabOpen = false;
+    private TextView mTextViewDescription, mTextViewRecommend1, mTextViewRecommend2, mTextViewRecommend3;
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -78,6 +89,36 @@ public class MainActivity
         final NavigationView leftNavigationView = findViewById(R.id.left_navigation);
         leftNavigationView.setNavigationItemSelectedListener(this);
 
+        TextView textViewTitle = findViewById(R.id.tv_main_title);
+        mTextViewDescription = findViewById(R.id.tv_main_description);
+        TextView textViewHashTags = findViewById(R.id.tv_main_hash_tags);
+        textViewTitle.setText("우아한 형제들");
+        mTextViewDescription.setText("‘좋은 음식을 먹고 싶은 곳에서’ 라는\n" +
+                "비전 아래 배달의민족, 배민라이더스, 배민찬 등을 서비스하며 종합 ‘푸드테크’ 기업으로\n" +
+                "나아가고 있습니다.\n" +
+                "\n" +
+                "우리는 ‘구성원을 행복하게 만들면 행복한 구성원이 더 좋은 서비스를 만든다’ 는 믿음으로 성장의 중심에는 코드 덩어리가 아닌 \u0003가치를 만들고 스스로 가치를 높이며 일하는\n" +
+                "우아한 개발자들이 있습니다.");
+        textViewHashTags.setText("#개발 #디자인 #기획 #해커톤 #오픈소스");
+
+        mTextViewRecommend1 = findViewById(R.id.tv_recommend_1);
+        mTextViewRecommend2 = findViewById(R.id.tv_recommend_2);
+        mTextViewRecommend3 = findViewById(R.id.tv_recommend_3);
+
+        dismissFabChild();
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isFabOpen) {
+                    showFabChild();
+                } else {
+                    dismissFabChild();
+                }
+            }
+        });
+
         mMainDialog = new MainDialog(this,
                 "2017 취업박람회",
                 "35년간 유학업계를 선도하며 \n" +
@@ -89,9 +130,30 @@ public class MainActivity
                         "총 10개국 어학연수 및 유학 상담이 가능합니다. 국가별 전문가와 1:1 상담을 받아보세요.",
                 dialogButtonListener);
         mMainDialog.show();
+
+
+        ScrollingValuePicker myScrollingValuePicker = findViewById(R.id.myScrollingValuePicker);
+        myScrollingValuePicker.setViewMultipleSize(0.9f);
+        myScrollingValuePicker.setMaxValue(0, 10);
+        myScrollingValuePicker.setValueTypeMultiple(1);
     }
 
+    private void showFabChild() {
+        mTextViewDescription.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark));
+        (findViewById(R.id.rl_layout_main)).bringToFront();
+        isFabOpen = false;
+        mTextViewRecommend1.animate().setDuration(100).scaleX(1);
+        mTextViewRecommend2.animate().setDuration(200).scaleX(1);
+        mTextViewRecommend3.animate().setDuration(300).scaleX(1);
+    }
 
+    private void dismissFabChild() {
+        mTextViewDescription.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.white));
+        isFabOpen = true;
+        mTextViewRecommend1.animate().setDuration(300).scaleX(0);
+        mTextViewRecommend2.animate().setDuration(200).scaleX(0);
+        mTextViewRecommend3.animate().setDuration(100).scaleX(0);
+    }
 
     @Override
     public void onBackPressed() {
