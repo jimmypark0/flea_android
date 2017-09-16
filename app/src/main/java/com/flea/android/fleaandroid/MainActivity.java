ad2a99559b04fbcfb03a1576987e433b7ee6eb4b
JerryJung-MacBook Pro <jwh1269@naver.com>
2017-09-16 10:55:30 +0900

    기본 환경설정 세팅 완료

--- app/src/main/java/com/flea/android/fleaandroid/MainActivity.java
+++ app/src/main/java/com/flea/android/fleaandroid/MainActivity.java
@@ -1,9 +1,9 @@
 package com.flea.android.fleaandroid;
 
 import android.content.DialogInterface;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,21 +15,19 @@ import com.estimote.sdk.SystemRequirementsChecker;
 import java.util.HashMap;
 import java.util.List;
 
-import static android.media.CamcorderProfile.get;
-
 public class MainActivity extends AppCompatActivity {
-    private BeaconManager beaconManager;
+    /*private BeaconManager beaconManager;
     private static final Region ALL_ESTIMOTE_BEACONS_REGION = new Region("rid", null, null, null);
 
     private TextView tvId;
-    HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+    HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvId = (TextView) findViewById(R.id.tvId);
+        /*tvId = (TextView) findViewById(R.id.tvId);
 
         beaconManager = new BeaconManager(getApplicationContext());
 
@@ -74,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             }
-        });
+        });*/
     }
 
     @Override
@@ -82,12 +80,12 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         // 블루투스 권한 및 활성화 코드
-        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+        //SystemRequirementsChecker.checkWithDefaultDialogs(this);
     }
 
     @Override
     protected void onPause() {
-        beaconManager.stopRanging(ALL_ESTIMOTE_BEACONS_REGION);
+        //beaconManager.stopRanging(ALL_ESTIMOTE_BEACONS_REGION);
         super.onPause();
     }
 }
